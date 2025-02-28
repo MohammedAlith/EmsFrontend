a03c729aa02461f5116357c3f1efa317bd1a8fe0
Smda@1312 <mohammedalith713@gmail.com>
2025-02-28 19:20:19 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  server: {
-    port: 3000
-  }
   
-})
+  server: {
+    host: true, // Allow external access
+    port: 3000, // You can keep 3000 if needed
+    strictPort: true,
+  },
+
+  preview: {
+    allowedHosts: ['emsfrontend-production-ae35.up.railway.app'], // Allow Railway host
+  }
+});
